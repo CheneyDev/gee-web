158be9383d30c572df051f3c25493d422fcb8434
CheneyDev <cheneydev@gmail.com>
2023-11-08 18:46:10 +0800

    context.go

--- gee/context.go
+++ gee/context.go
@@ -12,6 +12,7 @@ type Context struct {
 	Req        *http.Request
 	Path       string
 	Method     string
+	Params     map[string]string
 	StatusCode int
 }
 
@@ -22,6 +23,12 @@ func newContext(w http.ResponseWriter, r *http.Request) *Context {
 		Path:   r.URL.Path,
 		Method: r.Method,
 	}
+}
+
+func (c *Context) Param(key string) string {
+	value, _ := c.Params[key]
+	return value
+
 }
 
 func (c *Context) PostForm(key string) string {
