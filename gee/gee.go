0e3a0f2108d4293a0886a2dfa97288a055f09fe0
CheneyDev <cheneydev@gmail.com>
2023-11-08 18:20:23 +0800

    trie.go

--- gee/gee.go
+++ gee/gee.go
@@ -5,12 +5,6 @@ import (
 	"net/http"
 )
 
-// HandlerFunc 是一个自定义类型。
-// 具体来说，HandlerFunc 是一个函数类型，它接受两个参数，分别是 http.ResponseWriter 和 *http.Request。
-// 主要用于定义 HTTP 请求处理函数的格式。在许多 Web 开发框架中，HTTP 请求处理函数通常需要遵循特定的签名，
-// 即接受一个 http.ResponseWriter 用于写入 HTTP 响应，以及一个 *http.Request 用于表示 HTTP 请求。
-// 通过定义 HandlerFunc 类型，可以使代码更加模块化和可扩展，允许你在不修改函数签名的情况下更改或替换处理函数。
-// 这有助于提高代码的灵活性和可维护性，因为你可以轻松地在不同地方使用相同的函数类型，而无需担心函数签名的不匹配。
 type HandlerFunc func(*Context)
 
 type Engine struct {
--- main.go
+++ main.go
@@ -1,20 +1,24 @@
 package main
 
 import (
-	"fmt"
 	"gee"
 	"net/http"
 )
 
 func main() {
 	r := gee.New()
-	r.GET("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+	r.GET("/", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello World!</h1>")
 	})
-	r.GET("/hello", func(w http.ResponseWriter, r *http.Request) {
-		for k, v := range r.Header {
-			fmt.Fprintf(w, "Header [%q]= %q\n", k, v)
-		}
+	r.GET("/hello", func(c *gee.Context) {
+		c.String(http.StatusOK, "hello %s, you are at %s", c.Query("name"), c.Path)
+	})
+
+	r.POST("/login", func(c *gee.Context) {
+		c.JSON(http.StatusOK, gee.H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
 	})
 
 	r.Run(":9955")
